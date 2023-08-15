a17d8a8850b5abccd612feb27a75a6338221687d
Phuc Long Nguyen <90314029+longcg18@users.noreply.github.com>
2023-08-15 10:57:27 +0700

    BE Update: Add dotenv

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,30 +5,24 @@ import { LocationModule } from './location/location.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DeviceModule } from './device/device.module';
 import { RelationModule } from './relation/relation.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type:'mysql',
-      host: 'localhost',
-      port: +'3307',
-      username: 'root',
-      password: '20021387',
+      host: process.env.DB_HOST,
+      port: + (process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
       database: 'nism',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
-    
-    
-    
     LocationModule,
-    
-    
-    
     DeviceModule,
-    
-    
-    
     RelationModule],
   controllers: [AppController],
   providers: [AppService],
